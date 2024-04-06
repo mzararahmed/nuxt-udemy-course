f4eb5d39c884ecc9708eca5af1422a14bf68901d
mzararahmed <md.zarar.ahmed@gmail.com>
2024-04-06 16:09:10 +0500

    app footer changed

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <p>Footer</p>
+  <div class="site-footer">
+    <footer>
+      <h6 headline>Invotyx Academy © {{ new Date().getFullYear() }} </h6>
+    </footer>
   </div>
 </template>
+
+
+<style scoped>
+.site-footer {
+  background-color: #26272b;
+  margin-top: 10px;
+  padding: 4px;
+  font-size: 15px;
+  line-height: 24px;
+  color: #737373;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+}
+</style>
