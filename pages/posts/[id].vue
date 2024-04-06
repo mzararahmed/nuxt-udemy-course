41d98548f13793c56ab6fda2c47a436a95b59d5d
mzararahmed <md.zarar.ahmed@gmail.com>
2024-04-06 15:58:22 +0500

    border styles in post file

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,10 +1,24 @@
 <template>
-  <p :id="route.params.id" v-if=true>Post {{ route.params.id }}</p>
-  <p>Showing some raw html: <span v-html="rawHtml"></span></p>
+  <div :class="border">
+    <p :id="route.params.id" v-if="false === false">Post {{ route.params.id }}</p>
+    <p>Showing some raw html: <span v-html="rawHtml"></span></p>
+  </div>
+
 </template>
 
 <script setup>
 const route = useRoute();
 
 let rawHtml = '<span style="color:blue"> This should be blue </span>'
+
+const truthyValue = () => {
+  return false
+}
 </script>
+
+<style scoped>
+div {
+  border: 1px solid black;
+  padding: 5px;
+}
+</style>
