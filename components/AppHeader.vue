142e07e9a8b1b4c2773bc81586b2c950b2afc5e9
mzararahmed <md.zarar.ahmed@gmail.com>
2024-04-06 08:22:17 +0500

    dynamic routes and navigation

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <p>Header</p>
-  </div>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/home">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/posts/1">Post 1</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/posts/2">Post 2</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,8 @@
+<template>
+  <p>Post</p>
+</template>
+<script setup>
+const route = useRoute();
+
+console.log("Route parameter:", route.params.id)
+</script>
