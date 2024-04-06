dfd9b6134f24759d76c5bf228061b2b942ca557a
mzararahmed <md.zarar.ahmed@gmail.com>
2024-04-06 15:01:17 +0500

    v-if issues

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,8 +1,10 @@
 <template>
-  <p>Post</p>
+  <p :id="route.params.id" v-if=true>Post {{ route.params.id }}</p>
+  <p>Showing some raw html: <span v-html="rawHtml"></span></p>
 </template>
+
 <script setup>
 const route = useRoute();
 
-console.log("Route parameter:", route.params.id)
+let rawHtml = '<span style="color:blue"> This should be blue </span>'
 </script>
